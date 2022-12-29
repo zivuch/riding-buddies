80a5fd53cb4413d56f32ad6a9ce8d3035b5de819
zivuch <zivuch@gmail.com>
2022-12-30 00:40:52 +0200

    first commit

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -1,58 +1,58 @@
-import {Sequelize} from 'sequelize';
-import db from '../config/elephantSql.js';
+import { Sequelize } from "sequelize";
+import db from "../config/elephantSQL.js";
 
-const {DataTypes} = Sequelize;
+const { DataTypes } = Sequelize;
 
 //Model Instances
-const Users = db.define('users', {
-        firstName: {
-            field: 'firstname',
-        type:DataTypes.STRING,
-        allowNull: false,
+const Users = db.define(
+  "users",
+  {
+    firstName: {
+      field: "firstname",
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-        lastname: {
-        type:DataTypes.STRING,
-        allowNull: false,
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-        type:DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     gender: {
-        type:DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     phoneNumber: {
-        field: 'number',
-        type:DataTypes.STRING,
-        allowNull: false,
+      field: "number",
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     cityOfResidence: {
-        field: 'city',
-        type:DataTypes.STRING,
-        allowNull: false,
+      field: "city",
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     motorcycle: {
-        type:DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    
+
     lat: {
-        type:DataTypes.STRING,
-        
+      type: DataTypes.STRING,
     },
     lng: {
-        type:DataTypes.STRING,
-        
+      type: DataTypes.STRING,
     },
-    password:{
-        type:DataTypes.STRING,
-        allowNull: false,
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-},
-{
-    timestamps: false
-}
-)
+  },
+  {
+    timestamps: false,
+  }
+);
 
 export default Users;
